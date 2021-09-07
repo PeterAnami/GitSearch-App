706efe76ca1ba5b4763f879fd2bccee5c05c5cb3
peterAnami <peter.andake@student.moringaschool.com>
2021-09-07 16:21:33 +0300

    changes

---
+++ src/app/date.pipe.ts
@@ -0,0 +1,10 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { DatePipe } from "@angular/common";
+
+@Pipe({ name: "customDateFormat" })
+export class CustomDateFormat implements PipeTransform {
+  transform(value: any): Date {
+    var customDate = new Date(value.match(/\d+/)[0] * 1);
+    return customDate;
+  }
+}
