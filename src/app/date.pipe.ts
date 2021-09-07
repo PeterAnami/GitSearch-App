e5341325cddd37490afcd1db69e5c6aa52c1215e
peterAnami <peter.andake@student.moringaschool.com>
2021-09-07 21:54:02 +0300

    Auto-generated commit [ci skip]

--- src/app/date.pipe.ts
+++
@@ -1,9 +0,0 @@
-import { Pipe, PipeTransform } from '@angular/core';
-
-@Pipe({ name: "customDateFormat" })
-export class CustomDateFormat implements PipeTransform {
-  transform(value: any): Date {
-    var customDate = new Date(value.match(/\d+/)[0] * 1);
-    return customDate;
-  }
-}
