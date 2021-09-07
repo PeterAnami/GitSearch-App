a4c5a5fc17c0fcf89be25588f0a9eb35362ab7b2
peterAnami <peter.andake@student.moringaschool.com>
2021-09-07 16:14:43 +0300

    changes

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProfileService } from "../profile.service";
+import {CustomDateFormat} from "../date.pipe";
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  profile: any;
+  repos: any;
+  username: string;
 
-  ngOnInit(): void {
+  constructor(private ProfileService: ProfileService) {
+
+    this.ProfileService.updateProfile(this.username);
+    this.ProfileService.getprofileInfo().subscribe(Profile => {
+      console.log(Profile);
+      this.profile = Profile;
+    });
+    this.ProfileService.getProfileRepos().subscribe(repos => {
+      console.log(repos);
+      this.repos = repos;
+    });
+  }
+  findProfile() {
+    this.ProfileService.updateProfile(this.username);
+    this.ProfileService.getprofileInfo().subscribe(Profile => {
+      console.log(Profile);
+      this.profile = Profile;
+    });
+    this.ProfileService.getProfileRepos().subscribe(repos => {
+      console.log(repos);
+      this.repos = repos;
+    });
   }
 
+  ngOnInit() {}
 }
+
